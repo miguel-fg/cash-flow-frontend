caf2f2f322d0701c85869295797e8d9acf4a1283
miguel-fg <miguelfierro55@gmail.com>
2023-09-27 21:58:57 -0700

    Added user state persistence with local storage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,38 +10,71 @@ import Button from "react-bootstrap/Button";
 // logout custom hook
 import { useLogout } from "../hooks/useLogout";
 
+// user context
+import { useAuthContext } from "../hooks/useAuthContext";
+
 export default function NavBar() {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
 
     const handleClick = () => {
         logout();
-    }
+    };
 
     // Web application navbar
     return (
         <>
-        <Navbar expand="lg" variant="light">
-            <Container fluid className="nav-container rounded">
-                <LinkContainer to="/" className="nav-title-link">
-                    <Navbar.Brand><span className="navbar-text cash-flow">Cash Flow</span></Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav className="justify-content-end flex-grow-1 pe-2">
-                        <LinkContainer to="/login" className="nav-link">
-                            <Nav.Link><span className="navbar-text">Login</span></Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/signup" className="nav-link">
-                            <Nav.Link><span className="navbar-text">Sign up</span></Nav.Link>
-                        </LinkContainer>
-                        <Button className="btn" onClick={handleClick}>Logout</Button>
-                        <LinkContainer to="/dashboard" className="nav-link">
-                            <Nav.Link><span className="navbar-text">Dashboard</span></Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar expand="lg" variant="light">
+                <Container fluid className="nav-container rounded">
+                    <LinkContainer to="/" className="nav-title-link">
+                        <Navbar.Brand>
+                            <span className="navbar-text cash-flow">
+                                Cash Flow
+                            </span>
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav className="justify-content-end flex-grow-1 pe-2">
+                            {!user && (
+                                <>
+                                    <LinkContainer
+                                        to="/login"
+                                        className="nav-link"
+                                    >
+                                        <Nav.Link>
+                                            <span className="navbar-text">
+                                                Login
+                                            </span>
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                    <LinkContainer
+                                        to="/signup"
+                                        className="nav-link"
+                                    >
+                                        <Nav.Link>
+                                            <span className="navbar-text">
+                                                Sign up
+                                            </span>
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                </>
+                            )}
+                            {user && (
+                                <div>
+                                    <span className="navbar-text">{user.email}</span>
+                                    <Button
+                                        className="btn logout"
+                                        onClick={handleClick}
+                                    >
+                                        Logout
+                                    </Button>
+                                </div>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     );
 }
